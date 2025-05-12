a6f5fd8045be7705d180bd45efd5f4c36b917b0f
Helen <150084608+brandoline@users.noreply.github.com>
2025-05-12 18:09:54 -0300

    Files update

---
+++ script.js
@@ -0,0 +1,21 @@
+        // números
+        let numeroUm = document.getElementById("1");
+        let numeroDois = document.getElementById("2");
+        let numeroTres = document.getElementById("3");
+        let numeroQuatro = document.getElementById("4");
+        let numeroCinco = document.getElementById("5");
+        let numeroSeis = document.getElementById("6");
+        let numeroSete = document.getElementById("7");
+        let numeroOito = document.getElementById("8");
+        let numeroNove = document.getElementById("9");
+        let numeroZero = document.getElementById("0");
+
+        // operações
+        let mais = document.getElementById("+");
+        let menos = document.getElementById("-");
+        let divisao = document.getElementById("/");
+        let multiplicacao = document.getElementById("*");
+        let igual = document.getElementById("=");
+        let ponto = document.getElementById(".");
+
+        console.log(numeroUm);
